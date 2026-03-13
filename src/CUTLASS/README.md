8aea82d1ac131155cabc46a3647139ae03157d5e
haotianmichael <codesawmichael@gmail.com>
2026-03-13 10:44:16 +0800

    feat(hgemm): replace all macro with inline kernel

--- src/CUTLASS/README.md
+++ src/CUTLASS/README.md
@@ -1,6 +1,7 @@
 # CUTLASS
 
-* [CUTLASS源码导读](https://zhuanlan.zhihu.com/p/588953452)
-* [深入分析CUTLASS系列](https://zhuanlan.zhihu.com/p/677616101)
+* [CUTLASS源码导读-霸王手枪腿](https://zhuanlan.zhihu.com/p/588953452)
+* [CUTLASS笔记-杨远航](https://zhuanlan.zhihu.com/p/1937220431728845963)
+* [深入分析CUTLASS系列-JoeNomad](https://zhuanlan.zhihu.com/p/677616101)
 * [NV-CUTLASS](https://developer.nvidia.com/blog/cutlass-linear-algebra-cuda/)
-* [CUTE](https://declk.github.io/blog/CUDA/index.html)
+* [CUTE-Declk](https://declk.github.io/blog/CUDA/index.html)
--- src/HGEMM/hgemm_mma.cu
+++ src/HGEMM/hgemm_mma.cu
@@ -4,16 +4,6 @@
 #include "common/util.h"
 #include "ptx.cuh"
 
-#define LDST128BITS(pointer) (reinterpret_cast<float4 *>(&(pointer))[0])
-#define LDST32BITS(pointer) (reinterpret_cast<half2 *>(&(pointer))[0])
-#define CP_ASYNC_CA(dst, src, bytes) asm volatile("cp.async.ca.shared.global.L2::128B [%0], [%1], %2;\n" ::"r"(dst), "l"(src), "n"(bytes))
-#define CP_ASYNC_CG(dst, src, bytes) asm volatile("cp.async.cg.shared.global.L2::128B [%0], [%1], %2;\n" ::"r"(dst), "l"(src), "n"(bytes))
-#define CP_ASYNC_COMMIT_GROUP() asm volatile("cp.async.commit_group;\n" ::)
-#define CP_ASYNC_WAIT_ALL() asm volatile("cp.async.wait_all;\n" ::)
-#define CP_ASYNC_WAIT_GROUP(n) asm volatile("cp.async.wait_group %0;\n" ::"n"(n))
-#define LDMATRIX_X4(R0, R1, R2, R3, addr) asm volatile("ldmatrix.sync.aligned.x4.m8n8.shared.b16 {%0, %1, %2, %3}, [%4];\n" : "=r"(R0), "=r"(R1), "=r"(R2), "=r"(R3) : "r"(addr))
-#define LDMATRIX_X2_t(R0, R1, addr) asm volatile("ldmatrix.sync.aligned.x2.trans.m8n8.shared.b16 {%0, %1}, [%2];\n" : "=r"(R0), "=r"(R1) : "r"(addr))
-#define HMMA16816(RD0, RD1, RA0, RA1, RA2, RA3, RB0, RB1, RC0, RC1) asm volatile("mma.sync.aligned.m16n8k16.row.col.f16.f16.f16.f16 {%0, %1}, {%2, %3, %4, %5}, {%6, %7}, {%8, %9};\n" : "=r"(RD0), "=r"(RD1) : "r"(RA0), "r"(RA1), "r"(RA2), "r"(RA3), "r"(RB0), "r"(RB1), "r"(RC0), "r"(RC1))
 __device__ __forceinline__ void ld_st_128bit(void *dst, void *src) {
     *reinterpret_cast<float4 *>(dst) = *reinterpret_cast<float4 *>(src);
 } 
@@ -53,41 +43,37 @@ __global__ void hgemm_mma_m16n8k16_kernel_v1(half *A, half *B, half *C, unsigned
     const int load_gmem_b_n = bx * BN + load_smem_b_n;
     if(load_gmem_a_m >= M && load_gmem_b_n >= N) return;
 
-    uint32_t RC[2] = {0, 0};
+    half RC[4] = {0, 0, 0, 0};
     for(int k = 0; k < NUM_K_TILES; k ++) {
         int load_gmem_a_k = k * BK + load_smem_a_k;
         int load_gmem_a_addr = load_gmem_a_m * K + load_gmem_a_k;
-        LDST128BITS(tileA[load_smem_a_m][load_smem_a_k]) = 
-            LDST128BITS(A[load_gmem_a_addr]);
+        ld_st_128bit(&tileA[load_smem_a_m][load_smem_a_k], &A[load_gmem_a_addr]);
         
         if(lane_id < MMA_K) {
             int load_gmem_b_k = k * MMA_K + load_smem_b_k;
             int load_gmem_b_addr = load_gmem_b_k * N + load_gmem_b_n;
-            LDST128BITS(tileB[load_smem_b_k][load_smem_b_n]) = LDST128BITS(B[load_gmem_b_addr]);
+            ld_st_128bit(&tileB[load_smem_b_k][load_smem_b_n], &B[load_gmem_b_addr]);
         }
         __syncthreads();
 
-        uint32_t RA[4];
-        uint32_t RB[2];
+        half RA[8];
+        half RB[4];
 
-        uint32_t load_smem_a_ptr = __cvta_generic_to_shared(&tileA[lane_id % 16][(lane_id / 16) * 8]);
-        LDMATRIX_X4(RA[0], RA[1], RA[2], RA[3], load_smem_a_ptr);
-        uint32_t load_smem_b_ptr = __cvta_generic_to_shared(&tileB[lane_id % 16][0]);
-        LDMATRIX_X2_t(RB[0], RB[1], load_smem_b_ptr);
-
-        HMMA16816(RC[0], RC[1], RA[0], RA[1], RA[2], RA[3], RB[0], RB[1], RC[0], RC[1]);
+        ptx::ldmatrix_sync(RA, &tileA[lane_id % 16][(lane_id/16)*8]);
+        ptx::ldmatrix_sync(RB, &tileB[lane_id % 16][0]);
+        ptx::mma_sync_m16n8k16(RC, RA, RB);
         __syncthreads();
     }
 
-    LDST32BITS(tileC[lane_id / 4][(lane_id % 4) * 2]) = LDST32BITS(RC[0]);
-    LDST32BITS(tileC[lane_id / 4 + 8][(lane_id % 4) * 2]) = LDST32BITS(RC[1]);
+    ld_st_32bit(&tileC[lane_id / 4][(lane_id % 4) * 2], &RC[0]);
+    ld_st_32bit(&tileC[lane_id / 4 + 8][((lane_id % 4) * 2)], &RC[1]);
 
     __syncthreads();
     if(lane_id < MMA_M) {
         int store_gmem_c_m = by * BM + lane_id;
         int store_gmem_c_n = bx * BN;
         int store_gmem_c_addr = store_gmem_c_m * N + store_gmem_c_n;
-        LDST128BITS(C[store_gmem_c_addr]) = LDST128BITS(tileC[lane_id][0]);
+        ld_st_128bit(&C[store_gmem_c_addr], &tileC[lane_id][0]);
     }
     return;
 }
